9d6caeb05f51355c440dfbd396a37247da58559c
sjyzheng <zjysteve@gmail.com>
2020-03-18 16:56:36 -0700

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CourseListComponent } from './course-list/course-list.component';
+import { CourseServiceClient } from './services/CourseServiceClient';
+import { FormsModule } from '@angular/forms';
+import { CourseTableComponent } from './course-table/course-table.component';
+import { CourseViewerComponent } from './course-viewer/course-viewer.component';
+import { ModuleListComponent } from './module-list/module-list.component';
+import { LessonTabsComponent } from './lesson-tabs/lesson-tabs.component';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { ModuleServiceClient } from './services/ModuleServiceClient';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseListComponent,
+    CourseTableComponent,
+    CourseViewerComponent,
+    ModuleListComponent,
+    LessonTabsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    CourseServiceClient,
+    ModuleServiceClient,
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
