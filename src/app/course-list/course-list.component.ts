afef041ac88280d34c768820fcf7be3f8c131132
sjyzheng <zjysteve@gmail.com>
2020-03-18 16:56:01 -0700

    Create CourseListComponent

---
+++ src/app/course-list/course-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {CourseServiceClient} from '../services/CourseServiceClient';
+
+@Component({
+  selector: 'app-course-list',
+  templateUrl: './course-list.component.html',
+  styleUrls: ['./course-list.component.css']
+})
+export class CourseListComponent implements OnInit {
+  courses = [];
+
+  constructor(private service: CourseServiceClient) { }
+
+  ngOnInit(): void {
+    this.service.findAllCourses()
+      .then(courses => this.courses = courses);
+
+  }
+}
