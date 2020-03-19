1b7a3fca2adc30d86105a75b7a409239b1db0773
sjyzheng <zjysteve@gmail.com>
2020-03-19 00:53:49 -0700

    Update LessonTabsComponent to persist state

--- src/app/lesson-tabs/lesson-tabs.component.ts
+++ src/app/lesson-tabs/lesson-tabs.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { LessonServiceClient } from '../services/LessonServiceClient';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-lesson-tabs',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lesson-tabs.component.css']
 })
 export class LessonTabsComponent implements OnInit {
+  lessons = [];
+  @Input() course;
+  @Input() modules;
+  courseId = '';
+  moduleId = '';
+  lessonId = '';
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private service: LessonServiceClient, private router: Router) {
+    this.lessons = this.router.getCurrentNavigation().extras.state ?
+      this.router.getCurrentNavigation().extras.state.lessons : [] ;
+  }
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.courseId = params.courseId;
+      this.moduleId = params.moduleId;
+      this.lessonId = params.lessonId;
+      if (this.moduleId) { this.service.findLessonsForModule(this.moduleId)
+        .then(lessons => this.lessons = lessons);
+      }
+    });
   }
 
 }
