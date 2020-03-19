3cbaac67800331e6f3d2cd4e2cc0a7eb5c562c7c
sjyzheng <zjysteve@gmail.com>
2020-03-19 00:53:26 -0700

    Add lesson service

--- src/app/services/LessonServiceClient.ts
+++ src/app/services/LessonServiceClient.ts
@@ -2,5 +2,8 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class LessonServiceClient {
+  findLessonsForModule = (moduleId) =>
+    fetch(`https://wbdv-sp20-jzheng-server-java.herokuapp.com/api/modules/${moduleId}/lessons`)
+      .then(response => response.json())
 
 }
