152080d2f433496c53c6662abe0d1379a0612102
sjyzheng <zjysteve@gmail.com>
2020-03-19 00:54:04 -0700

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,14 +6,12 @@ import { CourseViewerComponent } from './course-viewer/course-viewer.component';
 const routes: Routes = [
   {path: '', component: CourseListComponent},
   {path: 'course-viewer/:courseId', component: CourseViewerComponent},
-  {path: 'course-viewer/:courseId/modules/:moduleId', component: CourseViewerComponent}
-
-]
+  {path: 'course-viewer/:courseId/modules/:moduleId', component: CourseViewerComponent},
+  {path: 'course-viewer/:courseId/modules/:moduleId/lessons/:lessonId', component: CourseViewerComponent}
+];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes)
-  ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
