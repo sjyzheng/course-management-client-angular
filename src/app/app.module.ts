552fcb3a8697b989c08841eb08fa628c32d040cb
sjyzheng <zjysteve@gmail.com>
2020-03-19 00:53:57 -0700

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,18 @@ import { AppComponent } from './app.component';
 import { CourseListComponent } from './course-list/course-list.component';
 import { CourseServiceClient } from './services/CourseServiceClient';
 import { FormsModule } from '@angular/forms';
-import { CourseTableComponent } from './course-table/course-table.component';
 import { CourseViewerComponent } from './course-viewer/course-viewer.component';
 import { ModuleListComponent } from './module-list/module-list.component';
 import { LessonTabsComponent } from './lesson-tabs/lesson-tabs.component';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { ModuleServiceClient } from './services/ModuleServiceClient';
+import { LessonServiceClient } from './services/LessonServiceClient';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
-    CourseTableComponent,
     CourseViewerComponent,
     ModuleListComponent,
     LessonTabsComponent
@@ -31,7 +30,7 @@ import { ModuleServiceClient } from './services/ModuleServiceClient';
   providers: [
     CourseServiceClient,
     ModuleServiceClient,
-
+    LessonServiceClient
   ],
   bootstrap: [AppComponent]
 })
