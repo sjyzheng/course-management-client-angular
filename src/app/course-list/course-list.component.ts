404c75fb3aad5a301fbc4be3001acd321efb2487
sjyzheng <zjysteve@gmail.com>
2020-03-19 21:22:59 -0700

    Add course search function

--- src/app/course-list/course-list.component.ts
+++ src/app/course-list/course-list.component.ts
@@ -8,12 +8,19 @@ import {CourseServiceClient} from '../services/CourseServiceClient';
 })
 export class CourseListComponent implements OnInit {
   courses: any = [];
+  courseTitle = '';
 
   constructor(private service: CourseServiceClient) { }
 
   ngOnInit(): void {
     this.service.findAllCourses()
       .then(courses => this.courses = courses);
+  }
 
+  searchCourse(courseTitle: string) {
+    this.service.findAllCourses()
+      .then(courses => courseTitle === '' ?
+        courses : courses.filter(course => course.title.toLowerCase().includes(courseTitle.toLowerCase())))
+      .then(courses => this.courses = courses);
   }
 }
