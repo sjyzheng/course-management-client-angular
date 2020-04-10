1399a915f41a4eb35c473d98dda2864e55f749fe
Steve <zjysteve@gmail.com>
2020-04-10 17:51:35 -0600

    Create QuizServiceClient.ts

---
+++ src/app/services/QuizServiceClient.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class QuizServiceClient {
+  findAllQuizzes = () =>
+    fetch('http://localhost:3000/api/quizzes')
+      .then(response => response.json())
+  findQuizById = (qid) =>
+    fetch(`http://localhost:3000/api/quizzes/${qid}`)
+      .then(response => response.json())
+}
