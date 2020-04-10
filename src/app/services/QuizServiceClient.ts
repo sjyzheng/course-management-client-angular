74e9abd42e22f20a87ab746dc4fb887856f6def3
Steve <zjysteve@gmail.com>
2020-04-10 17:58:10 -0600

    Change URLs to the server on Heroku

--- src/app/services/QuizServiceClient.ts
+++ src/app/services/QuizServiceClient.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class QuizServiceClient {
   findAllQuizzes = () =>
-    fetch('http://localhost:3000/api/quizzes')
+    fetch('https://wbdv-sp20-jzheng-server-node.herokuapp.com/api/quizzes')
       .then(response => response.json())
   findQuizById = (qid) =>
     fetch(`http://localhost:3000/api/quizzes/${qid}`)
