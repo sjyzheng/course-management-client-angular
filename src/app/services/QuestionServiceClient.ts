5b230d937690ccc230cf8b4116a01295ec29c0e6
Steve <zjysteve@gmail.com>
2020-04-15 00:02:53 -0600

    Update QuestionServiceClient.ts

--- src/app/services/QuestionServiceClient.ts
+++ src/app/services/QuestionServiceClient.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class QuestionServiceClient {
   findQuestionsForQuiz = (qid) =>
-    fetch(`https://wbdv-sp20-jzheng-server-node.herokuapp.com/api/quizzes/${qid}/questions`)
+    fetch(`http://localhost:3000/api/quizzes/${qid}/questions`)
       .then(response => response.json())
 }
