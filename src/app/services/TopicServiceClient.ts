49bc6ecb9221920de7c75e2f895804b63be3ae64
Steve <zjysteve@gmail.com>
2020-04-15 00:02:48 -0600

    Update TopicServiceClient.ts

--- src/app/services/TopicServiceClient.ts
+++ src/app/services/TopicServiceClient.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class TopicServiceClient {
   findTopicsForLesson = (lessonId) =>
-    fetch(`https://wbdv-sp20-jzheng-server-java.herokuapp.com/api/lessons/${lessonId}/topics`)
+    fetch(`http://localhost:3000/api/lessons/${lessonId}/topics`)
       .then(response => response.json())
 }
