0d4899a9c9bff25b4b2d7c76815d585fe45eaf74
Steve <zjysteve@gmail.com>
2020-04-15 00:04:01 -0600

    Update multiple-choice-question.component.ts

--- src/app/quizzes/quiz/questions/multiple-choice-question/multiple-choice-question.component.ts
+++ src/app/quizzes/quiz/questions/multiple-choice-question/multiple-choice-question.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-multiple-choice-question',
@@ -6,10 +6,12 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./multiple-choice-question.component.css']
 })
 export class MultipleChoiceQuestionComponent implements OnInit {
-  @Input() question;
-  answer = '';
+  @Input() question  = {_id: '', title: '', type: '', choices: [], correct: '', question: ''};
+  @Input() answer = '';
   grading = false;
   wrongAnswer = '';
+  @Output()
+  answerChange = new EventEmitter<string>()
 
   constructor() { }
 
@@ -19,4 +21,11 @@ export class MultipleChoiceQuestionComponent implements OnInit {
     this.grading = true;
     if (this.answer !== this.question.correct) { this.wrongAnswer = this.answer; }
     if (this.answer === '') { this.answer = 'NOT ANSWERED'; }
-  }}
+  }
+  submitAnswer() {
+    this.answerChange.emit(this.answer)
+  }
+}
+
+
+
